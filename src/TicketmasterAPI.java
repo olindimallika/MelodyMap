bc8d6a6b45fd752c7a99054f44f185418c2ba0e4
Bea Castro <bea.castro@utoronto.ca>
2023-11-16 19:39:14 -0500

    adding apiKey instance variable

--- src/TicketmasterAPI.java
+++ src/TicketmasterAPI.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class TicketmasterAPI extends LocationFinder{
 
+    private final String apiKey;
+
     public TicketmasterAPI(String apiKey) {
         this.apiKey = apiKey;
     }
