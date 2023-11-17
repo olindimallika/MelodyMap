6e7dadc810fd9c42b231593452ac876671258af7
Bea Castro <bea.castro@utoronto.ca>
2023-11-16 20:23:27 -0500

    adding input and output data

--- src/use_case/upcoming_shows/UpcomingOutputData.java
+++ src/use_case/upcoming_shows/UpcomingOutputData.java
@@ -1,4 +1,17 @@
 package use_case.upcoming_shows;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class UpcomingOutputData {
+
+    private Map<String, String> upcomingConcerts = new HashMap<>();
+
+    public UpcomingOutputData(HashMap<String, String> upcomingConcerts){
+        this.upcomingConcerts = upcomingConcerts;
+    }
+
+    public Map<String, String> getUpcomingConcerts() {
+        return upcomingConcerts;
+    }
 }
