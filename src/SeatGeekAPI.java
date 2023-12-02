773b2827d591e14d11ffdf7db184511cb2d9041a
Bea Castro <bea.castro@mail.utoronto.ca>
2023-12-01 22:53:47 -0500

    adding file that gets upcoming event for artist

--- src/SeatGeekAPI.java
+++ src/SeatGeekAPI.java
@@ -9,6 +9,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 // returning taylor swift info https://api.seatgeek.com/2/performers?slug=taylor-swift&Mzg2MzEwODZ8MTcwMTM3MjE3Ny43MzQwMTQ3
 public class SeatGeekAPI {
@@ -21,38 +23,47 @@ public class SeatGeekAPI {
             String baseUrl = "https://api.seatgeek.com/2/performers?";
             String apiUrl = baseUrl + "slug=" + artistName + "&client_id=" + apiKey;
 
+            // for debugging purposes
+            System.out.println(apiUrl);
+
+
             // Create URL object
             URL url = new URL(apiUrl);
 
             // Open connection
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
+            //logging URL
+            System.out.println("Request URL: " + connection.getURL());
+
             // Set request method
             connection.setRequestMethod("GET");
 
             // Read the response
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
-            StringBuilder response = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String line;
+                StringBuilder response = new StringBuilder();
 
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                // Parse the JSON response
+                JSONObject jsonResponse = new JSONObject(response.toString());
+
+                JSONArray artistArray = (JSONArray) jsonResponse.get("performers");
+                JSONObject artistInfo = artistArray.getJSONObject(0);
+
+                Integer numUpcomingEvents = (Integer) artistInfo.get("num_upcoming_events");
+
+                if (artistInfo.get("has_upcoming_events").equals(true)) {
+                    System.out.println("Your favourite artist has " + numUpcomingEvents + " upcoming concerts!\nPurchase tickets here: " + artistInfo.get("url"));
+                } else {
+                    System.out.println("Sorry! Your favourite artist doesn't have any upcoming concerts :(");
+                }
             }
-            reader.close();
 
-            // Parse the JSON response
-            JSONObject jsonResponse = new JSONObject(response.toString());
-
-            JSONArray artistArray = (JSONArray) jsonResponse.get("performers");
-            JSONObject artistInfo = artistArray.getJSONObject(0);
-
-            Integer numUpcomingEvents = (Integer) artistInfo.get("num_upcoming_events");
-
-            if (artistInfo.get("has_upcoming_events").equals(true)) {
-                System.out.println("Your favourite artist has " + numUpcomingEvents + " upcoming concerts!\nPurchase tickets here: " +  artistInfo.get("url"));
-            } else {
-                System.out.println("Sorry! Your favourite artist doesn't have any upcoming concerts :(");
-            }
 
             // Close the connection
             connection.disconnect();
@@ -63,9 +74,10 @@ public class SeatGeekAPI {
 
     }
 
+
     public static void main(String[] args) {
         SeatGeekAPI seatGeekAPI = new SeatGeekAPI();
-        seatGeekAPI.notifyUser("seventeen");
+        seatGeekAPI.notifyUser("taylor-swift");
     }
 
 }
