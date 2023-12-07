cd316f780d7a07a05e8635a0bcd9eb9f973ba696
olindimallika <127895847+olindimallika@users.noreply.github.com>
2023-12-07 11:31:21 -0500

    Update SimilarArtistTest.java
    I think you missed adding a new line or \n to your expected output.

--- test/interactor/SimilarArtistTest.java
+++ test/interactor/SimilarArtistTest.java
@@ -15,11 +15,11 @@ public class SimilarArtistTest {
         SimilarOutputBoundary successPresenter = new SimilarOutputBoundary() {
             public void prepareSuccessView(SimilarOutputData user) {
                 String outputSimilarArtists = "Similar artist for Laufey: Matt Maltese https://www.ticketmaster.ca/matt-maltese-touring-just-to-tour-toronto-ontario-03-27-2024/event/10005F36A74D2478\n" +
-                        "Similar artist for Laufey: Ricky Montgomery https://www.ticketmaster.ca/ricky-montgomery-the-rick-tour-another-toronto-ontario-03-01-2024/event/10005F2A0D7B5DF8\n" +
-                        "Similar artist for Laufey: PinkPantheress https://www.ticketmaster.ca/pinkpantheress-capable-of-love-tour-toronto-ontario-04-07-2024/event/10005F69D0F62EF5\n" +
-                        "Similar artist for Taylor-Swift: Olivia Rodrigo https://www.ticketmaster.ca/olivia-rodrigo-guts-world-tour-toronto-ontario-03-29-2024/event/10005F2839E1667D\n" +
-                        "Similar artist for Niki: Doja Cat https://www.ticketmaster.ca/doja-cat-the-scarlet-tour-toronto-ontario-12-11-2023/event/10005ED4D89C2AB2\n" +
-                        "Similar artist for Niki: Ice Spice https://www.ticketmaster.ca/doja-cat-the-scarlet-tour-toronto-ontario-12-11-2023/event/10005ED4D89C2AB2\n";
+                        "Similar artist for Laufey: Ricky Montgomery https://www.ticketmaster.ca/ricky-montgomery-the-rick-tour-another-toronto-ontario-03-01-2024/event/10005F2A0D7B5DF8" +
+                        "Similar artist for Laufey: PinkPantheress https://www.ticketmaster.ca/pinkpantheress-capable-of-love-tour-toronto-ontario-04-07-2024/event/10005F69D0F62EF5" +
+                        "Similar artist for Taylor-Swift: Olivia Rodrigo https://www.ticketmaster.ca/olivia-rodrigo-guts-world-tour-toronto-ontario-03-29-2024/event/10005F2839E1667D" +
+                        "Similar artist for Niki: Doja Cat https://www.ticketmaster.ca/doja-cat-the-scarlet-tour-toronto-ontario-12-11-2023/event/10005ED4D89C2AB2" +
+                        "Similar artist for Niki: Ice Spice https://www.ticketmaster.ca/doja-cat-the-scarlet-tour-toronto-ontario-12-11-2023/event/10005ED4D89C2AB2";
                 assertEquals(outputSimilarArtists, user.getFormatOutputSimilarArtist());
             }
 
