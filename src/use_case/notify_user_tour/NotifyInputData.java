ce02057a720678c73411f534502c9d8555fd7089
Brittany <brittany.lansang@gmail.com>
2023-12-07 15:10:15 -0500

    Adding Javadoc to Notify Use case

--- src/use_case/notify_user_tour/NotifyInputData.java
+++ src/use_case/notify_user_tour/NotifyInputData.java
@@ -1,13 +1,31 @@
 package use_case.notify_user_tour;
 
+/**
+ * The {@code NotifyInputData} class represents the input data for the "Notify User Tour" use case.
+ * It encapsulates information about the user's favorite artist names.
+ */
 public class NotifyInputData {
-    final private String favouriteArtistNames;
 
-    public NotifyInputData(String favouriteArtistNames){
+    /**
+     * The favorite artist names provided as input.
+     */
+    private final String favouriteArtistNames;
+
+    /**
+     * Constructs a new {@code NotifyInputData} object with the specified favorite artist names.
+     *
+     * @param favouriteArtistNames The names of the user's favorite artists.
+     */
+    public NotifyInputData(String favouriteArtistNames) {
         this.favouriteArtistNames = favouriteArtistNames;
     }
 
-    public String getFavouriteArtistNames(){
+    /**
+     * Gets the favorite artist names from the input data.
+     *
+     * @return The names of the user's favorite artists.
+     */
+    public String getFavouriteArtistNames() {
         return favouriteArtistNames;
     }
 }
--- src/use_case/notify_user_tour/NotifyOutputData.java
+++ src/use_case/notify_user_tour/NotifyOutputData.java
@@ -1,17 +1,33 @@
 package use_case.notify_user_tour;
 
-import javax.swing.*;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
+/**
+ * The {@code NotifyOutputData} class represents the output data for the "Notify User Tour" use case.
+ * It encapsulates information about artists and their tour status to be presented to the user.
+ */
 public class NotifyOutputData {
+
+    /**
+     * A map containing artist names as keys and their corresponding tour status as values.
+     */
     private final LinkedHashMap<String, String> artistOnTour;
-    public NotifyOutputData(LinkedHashMap<String, String> artistOnTour){
+
+    /**
+     * Constructs a new {@code NotifyOutputData} object with the specified artist and tour status map.
+     *
+     * @param artistOnTour A map containing artist names and their corresponding tour status.
+     */
+    public NotifyOutputData(LinkedHashMap<String, String> artistOnTour) {
         this.artistOnTour = artistOnTour;
     }
 
-    public LinkedHashMap<String, String> getArtistOnTour(){
+    /**
+     * Gets the map containing artist names and their corresponding tour status.
+     *
+     * @return A map where each entry represents an artist name and its tour status.
+     */
+    public LinkedHashMap<String, String> getArtistOnTour() {
         return artistOnTour;
     }
-
 }
