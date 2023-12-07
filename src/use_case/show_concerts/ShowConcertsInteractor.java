1b5df6e307194c64b91cc61e16d3eebe1d999be9
kt <Kelsang.tsomo@mail.utoronto.ca>
2023-12-07 00:21:20 -0500

    javadoc for show concerts

--- src/use_case/show_concerts/ShowConcertsInteractor.java
+++ src/use_case/show_concerts/ShowConcertsInteractor.java
@@ -1,20 +1,61 @@
 package use_case.show_concerts;
 
-import java.util.ArrayList;
-
+/**
+ * The ShowConcertsInteractor class represents the interactor component for the show concerts use case.
+ * It implements the ShowConcertsInputBoundary interface, defining the behavior required to execute the show concerts logic.
+ *
+ * This class serves as the bridge between the application core and external layers, orchestrating the flow of information
+ * and interactions during the execution of the show concerts use case. It collaborates with the data access layer to fetch
+ * relevant information and communicates with the presentation layer to display the results to the user.
+ *
+ * The constructor of this class takes instances of ShowConcertsDataAccess and ShowConcertsOutputBoundary
+ * as parameters, establishing dependencies on the data access layer and the presenter. This adheres to the Dependency Inversion Principle,
+ * allowing the interactor to depend on abstractions rather than concrete implementations.
+ *
+ * The execute method, mandated by the ShowConcertsInputBoundary interface, is responsible for initiating
+ * the execution of the show concerts logic. Currently, it creates an instance of ShowConcertsOutputData and instructs
+ * the presenter to prepare a success view. In a real-world scenario, this method would involve interactions with the data access
+ * layer to fetch relevant concert information based on the input data.
+ *
+ * It is essential to note that this class is part of a larger use case, and the actual logic for fetching and processing
+ * concert information is expected to be implemented in collaborating classes, such as the data access objects and presenters.
+ *
+ * This class assumes a specific architecture and design pattern within the larger application, where components adhere to
+ * certain interfaces (ShowConcertsInputBoundary, ShowConcertsDataAccess, ShowConcertsOutputBoundary). Modifications to this class
+ * should be done with consideration for the overall application architecture and design.
+ *
+ * Author: Bea Castro
+ */
 public class ShowConcertsInteractor implements ShowConcertsInputBoundary{
+
+    // The data access object responsible for retrieving concert information
     final ShowConcertsDataAccess userDataAccessObject;
 
+    // The presenter responsible for preparing and displaying the results to the user
     final ShowConcertsOutputBoundary showConcertsPresenter;
 
+    /**
+     * Constructs a new instance of ShowConcertsInteractor
+     *
+     * @param userDataAccessInterface The data access object providing access to concert information.
+     * @param showConcertsOutputBoundary The presenter for displaying the results to the user.
+     */
     public ShowConcertsInteractor(ShowConcertsDataAccess userDataAccessInterface,
                            ShowConcertsOutputBoundary showConcertsOutputBoundary){
         this.userDataAccessObject = userDataAccessInterface;
         this.showConcertsPresenter = showConcertsOutputBoundary;
     }
 
+    /**
+     * Executes the show concerts logic based on the provided input data.
+     *
+     * @param showConcertsInputData The input data object encapsulating information necessary for the show concerts operation.
+     */
     public void execute(ShowConcertsInputData showConcertsInputData){
+        // Create an instance of ShowConcertsOutputData
         ShowConcertsOutputData showConcertsOutputData = new ShowConcertsOutputData();
+
+        // Instruct the presenter to prepare a success view based on the output data
         showConcertsPresenter.prepareSuccessView(showConcertsOutputData);
 
     }
--- src/use_case/show_concerts/ShowConcertsOutputBoundary.java
+++ src/use_case/show_concerts/ShowConcertsOutputBoundary.java
@@ -1,6 +1,37 @@
 package use_case.show_concerts;
 
+/**
+ * The ShowConcertsOutputBoundary interface defines the contract for presenting the results of the show concerts use case.
+ * Implementations of this interface are responsible for preparing and displaying views based on the outcome of the show concerts logic.
+ *
+ * This interface adheres to the Dependency Inversion Principle, serving as the boundary between the application core (use case)
+ * and the external layers (such as controllers and views). It abstracts the details of presentation, allowing the show concerts use case
+ * to remain decoupled from specific user interfaces or external display mechanisms.
+ *
+ * The two methods, prepareSuccessView and prepareFailView, handle the presentation of success and failure scenarios,
+ * respectively. Implementing classes should interpret the provided data and error information to create appropriate views for the user.
+ *
+ * It is crucial for implementations to handle the preparation of views efficiently and in a user-friendly manner. The success view
+ * may include details about upcoming concerts, while the fail view communicates errors or lack of available information.
+ *
+ * It is assumed that the actual implementations of this interface will interact with the user interface or other external
+ * systems to present the results of the show concerts use case. The specifics of view preparation are left to the implementing classes.
+ *
+ * Author: Bea Castro
+ */
 public interface ShowConcertsOutputBoundary {
+
+    /**
+     * Prepares and displays a success view based on the provided output data.
+     *
+     * @param ShowConcertsOutputData The output data object containing information to be presented in the success view.
+     */
     void prepareSuccessView(ShowConcertsOutputData user);
+
+    /**
+     * Prepares and displays a fail view based on the provided error message.
+     *
+     * @param error The error message describing the reason for the failure.
+     */
     void prepareFailView(String error);
 }
